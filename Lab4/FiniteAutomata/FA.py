22ccb6e0b874f45a4f97db85a4d955b931089f49
DaniMarcTheRebel <108721250+DaniMarcTheRebel@users.noreply.github.com>
2022-11-22 22:11:26 +0200

    ....

--- Lab4/FiniteAutomata/FA.py
+++ Lab4/FiniteAutomata/FA.py
@@ -26,6 +26,50 @@ class FiniteAutomata:
                     lineIndex += 1
 
 
+    def __printMenu(self):
+        print("\n1. Set of states")
+        print("2. The alphabet")
+        print("3. All of the transitions")
+        print("4. Initial state and final states")
+        print("0. Exit\n")
+        return
+
+
+    def __printStates(self):
+        print("\t>>>The set of states is: "+str(self.__states))
+
+
+    def __printAlphabet(self):
+        print("\t>>>The alphabet is:"+str(self.__alphabet))
+
+
+    def __printTransitions(self):
+        for trans in self.__transitions:
+            print("\t δ("+str(trans[0])+", "+str(trans[2])+") -> "+str(trans[1]))
+
+
+    def __printInitialFinal(self):
+        print("\t Initial state is: "+str(self.__initialState))
+        print("\t Final states are: "+str(self.__finalStates))
+
+
+    def FAMenu(self):
+        command = None
+        while True:
+            self.__printMenu()
+            command = int(input("Waiting for command: "))
+            if command == 1:
+                self.__printStates()
+            elif command == 2:
+                self.__printAlphabet()
+            elif command == 3:
+                self.__printTransitions()
+            elif command == 4:
+                self.__printInitialFinal()
+            elif command == 0:
+                break
+
+
     def getStates(self):
         return self.__states
 
@@ -49,5 +93,5 @@ class FiniteAutomata:
 
 
 FA = FiniteAutomata()
-print("read")
+FA.FAMenu()
     
